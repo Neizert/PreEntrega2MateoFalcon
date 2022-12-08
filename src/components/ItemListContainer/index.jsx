f474299a7f9d8f141936c8135fe37dc31909ad66
Mateo Falcon <neverxfulll@gmail.com>
2022-12-08 20:18:48 -0300

    Tercera versión

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Title from "../Title"
+import ItemCount from "../ItemCount";
 
 const ItemListContainer = () => {
+ 
+    const onAdd = (quantity) => {
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
     return (
-        <Title saludo="Bienvenido a mi pagina"/>
+        <div>
+        <Title saludo="Bienvenido a mi página"/>
+        <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+        </div>
     );
 };
 
